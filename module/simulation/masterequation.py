2ef2547f4dadd3e7baedf99dbc9fcfa9722de86f
Evan Wonisch <evanwonisch41@gmail.com>
2023-03-30 13:20:12 +0200

    commented master equation

--- module/simulation/masterequation.py
+++ module/simulation/masterequation.py
@@ -3,7 +3,17 @@ from module.base.network import Network
 import module.components.CONST as CONST
 
 class MasterEquation:
+    """
+    Solves the Master Equation for the single electron transistor
+    """
     def __init__(self, input_voltage, gate_voltage):
+        """
+        The output voltage is held fixed to zero.
+
+        Parameters:
+            input_voltage   : voltage of input electrode
+            gate_voltage    : voltage of gate  
+        """
         self.n = np.array([0])
         self.n2 = np.array([0.1])
 
@@ -17,15 +27,28 @@ class MasterEquation:
         self.build_A()
 
     def get_state(self, index):
+        """
+        Calculates the occupation number at a given index in phase space.
+        """
         return np.array([self.phase_space[index]])
     
     def get_rate_to_island(self, n):
+        """
+        Calculates the total rate towards the island for given occupation number.
+        """
         return self.net.calc_rate_from_electrode(n, 0) + self.net.calc_rate_from_electrode(n, 1)
     
     def get_rate_from_island(self, n):
+        """
+        Calculates the total rate towards the electrodes for given occupation number.
+        """
         return self.net.calc_rate_to_electrode(n, 0) + self.net.calc_rate_to_electrode(n, 1)
     
     def build_A(self):
+        """
+        The linear Master Equation is solved for a snippet of phase space.
+        Thus a Matrix characterising the dynamics is defined.
+        """
         for n in range(self.N):
 
             if not n == 0:
@@ -37,21 +60,35 @@ class MasterEquation:
                 self.A[n, n] += -self.get_rate_to_island(self.get_state(n))
 
     def evolve(self, probs, dt = 0.05):
+        """
+        Evolves a given prob vector of shape = phase_space.shape along dt in time.
+        """
         assert probs.shape == (self.N,)
         return probs + self.A @ probs * dt
     
     def convergence_metric(self, probs):
+        """
+        Returns the maxmimum rate of change for a probaility as convergence metric.
+        """
         return np.max(np.abs(self.A @ probs))
     
     def expected_current(self, probs):
-        rates = self.output_rate(np.expand_dims(self.phase_space, axis = -1))
+        """
+        For given probabilities, calculates the expected output current.
+        """
+        rates = self.output_current(np.expand_dims(self.phase_space, axis = -1))
         return np.sum(rates * probs)
         
-    def output_rate(self, state):
+    def output_current(self, state):
+        """
+        The output current as a function of phase-space.
+        """
         return -CONST.electron_charge * (self.net.calc_rate_from_electrode(state, 1) - self.net.calc_rate_to_electrode(state, 1))
     
     def solve(self, probs = None, N = 1500, verbose = False):
-
+        """
+        Solves the Master Equation for N iterations. Prints the convergence metric if verbose.
+        """
         if probs is None:
             probs = np.abs(np.random.randn(self.N))
             probs = probs / np.sum(probs)
